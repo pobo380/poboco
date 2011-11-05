3eda23d6c39121de9992ba33a98b14e1c2f3f238
pobo380 <n3560k@gmail.com>
2011-11-05 12:06:18 +0900

    Scene spec added.

---
+++ spec/scene_spec.rb
@@ -0,0 +1,11 @@
+# = RSpec test for Poboco
+module Poboco
+  # Test for Scene class
+  describe Scene do
+    share_examples_for 'Scene bsic function' do
+    end
+
+    describe '' do
+    end
+  end
+end
