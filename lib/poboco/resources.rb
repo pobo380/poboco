9330dbf352315fc329da9fe98e491c0b5a47c25f
pobo380 <n3560k@gmail.com>
2011-11-06 18:13:03 +0900

    Implemented Resouces#sounds and Resouces#graphics.

--- lib/poboco/resources.rb
+++ lib/poboco/resources.rb
@@ -10,6 +10,37 @@ module Poboco
   #
   class Resources
     include Singleton
+
+    def initialize
+      @sounds = {}
+      @graphics = {}
+    end
+
+    def sounds(key)
+      return @sounds[key] unless @sounds[key].nil?
+      ResourceLoader.new(lambda { |path, *args| @sounds[key] = Sound.new(path) })
+    end
+
+    def graphics(key, *args)
+      return @graphics[key] unless @graphics[key].nil?
+      ResourceLoader.new(lambda { |path, *args| @graphics[key] = Image.load(path, *args) })
+    end
+
+    class << self
+      def method_missing(action, *args)
+        self.instance.send(action, *args)
+      end
+    end
+
+    class ResourceLoader
+      def initialize(f)
+        @f = f
+      end
+
+      def load(path, *args)
+        @f.call(path, *args)
+      end
+    end
   end
 end
 
--- spec/resources_spec.rb
+++ spec/resources_spec.rb
@@ -3,8 +3,18 @@ require File.expand_path('../helper', __FILE__)
 module Poboco
   # = Test for Resources Class
   describe Resources do
-    it 'should be singleton.' do
-      expect { Resource.new }.to raise_error
+    describe 'should be singleton.' do
+      it 'raise error when Resources#new called.' do
+        expect { Resources.new }.to raise_error
+      end
+
+      it "should be able to load Sound." do
+        Resources.sounds(:click).load(File.expand_path('../resources/click.wav', __FILE__))
+      end
+
+      it "should be able to load Image." do
+        Resources.graphics(:button).load(File.expand_path('../resources/button.png', __FILE__))
+      end
     end
   end
 end
